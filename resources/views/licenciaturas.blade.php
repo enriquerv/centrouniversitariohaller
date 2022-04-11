18f2c1080e35c0c887283ceca7ef889f4bf992bf
Enrique Rodriguez <enrique.rv0704@gmail.com>
2022-04-11 00:33:32 -0500

    all

--- resources/views/licenciaturas.blade.php
+++ resources/views/licenciaturas.blade.php
@@ -14,25 +14,25 @@
 
       <div class="row">
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_admon.png" class="card_lic">
+          <a href="{{ URL::route('admon') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_admon.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_ciencias.png" class="card_lic">
+          <a href="{{ URL::route('ciencias') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_ciencias.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_comercio.png" class="card_lic">
+          <a href="{{ URL::route('comercio') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_comercio.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_derecho.png" class="card_lic">
+          <a href="{{ URL::route('derecho') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_derecho.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_gastro.png" class="card_lic">
+          <a href="{{ URL::route('gastro') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_gastro.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_merca.png" class="card_lic">
+          <a href="{{ URL::route('merca') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_merca.png" class="card_lic"></a>
         </div>
         <div class="col-md-4 text-center">
-          <img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_turismo.png" class="card_lic">
+          <a href="{{ URL::route('turismo') }}"><img src="{{ env('APP_URL') }}/assets/images2022/cards_lic_turismo.png" class="card_lic"></a>
         </div>
       </div>
 
